ccea46c68c0e7fd6daf1ae52cf1c39cf2a1111a6
99kenny <helloiamkenny@gmail.com>
2024-01-04 23:25:45 +0900

    Updates

---
+++ component---src-pages-about-tsx-20e708dae2ec49b1cb3b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[49],{3530:function(t,e,l){var n=l(4316),u=l(5907),s=l(4769),a=l(9978),r=l(917);const c=(0,n.Z)("div",{target:"e1strnqo0"})({name:"13ku56z",styles:"display:flex;flex-direction:column;height:100%"});e.Z=function(t){let{children:e}=t;return(0,r.tZ)(c,null,(0,r.tZ)(a.Z,null),(0,r.tZ)(u.Z,null),e,(0,r.tZ)(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-20e708dae2ec49b1cb3b.js.map
